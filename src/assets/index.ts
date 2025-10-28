3e5433f13a0458db1ab5f4a63da536d59f3a588c
Arijitkhan99git <arijitkhan52@gmail.com>
2025-10-28 13:25:46 +0530

    Fix image import casing

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,10 @@
 import jordan from './header/jordan.png'
 import nikeLogo from './header/nikeLogo.png'
 import banner1 from './banner/banner1.png'
-import airmax1 from './air/airmax1.png'
-import airmax2 from './air/airmax2.png'
-import airmax3 from './air/airmax3.png'
-import airforce1 from './air/airforce1.png'
-import airforce2 from './air/airforce2.png'
+import airMax1 from './air/airMax1.png'  // Changed: airmax1 → airMax1
+import airMax2 from './air/airMax2.png'  // Changed: airmax2 → airMax2
+import airMax3 from './air/airMax3.png'  // Changed: airmax3 → airMax3
+import airForce1 from './air/airForce1.png'  // Changed: airforce1 → airForce1
+import airForce2 from './air/airForce2.png'  // Changed: airforce2 → airForce2
 
-export {jordan, nikeLogo, banner1, airmax1, airmax2, airmax3, airforce1, airforce2}
+export {jordan, nikeLogo, banner1, airMax1, airMax2, airMax3, airForce1, airForce2}
