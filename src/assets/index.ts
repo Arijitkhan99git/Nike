e36c53c4bab96fd8b5a27ed825648e83b935c3a5
Arijitkhan99git <arijitkhan52@gmail.com>
2025-10-28 12:29:40 +0530

    updated camel casing

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,10 @@
 import jordan from '@/assets/header/jordan.png'
 import nikeLogo from '@/assets/header/nikeLogo.png'
 import banner1 from '@/assets/banner/banner1.png'
-import airMax1 from '@/assets/air/airMax1.png'
-import airMax2 from '@/assets/air/airMax2.png'
-import airMax3 from '@/assets/air/airMax3.png'
-import aiForce1 from '@/assets/air/airForce1.png'
-import airForce2 from '@/assets/air/airForce2.png'
+import airMax1 from '@/assets/air/airmax1.png'
+import airMax2 from '@/assets/air/airmax2.png'
+import airMax3 from '@/assets/air/airmax3.png'
+import aiForce1 from '@/assets/air/airforce1.png'
+import airForce2 from '@/assets/air/airforce2.png'
 
 export {jordan, nikeLogo, banner1, airMax1, airMax2, airMax3, aiForce1, airForce2}
