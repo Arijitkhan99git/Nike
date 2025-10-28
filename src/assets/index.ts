8e9e176b98019734d34310bc3ccf74b68976683b
Arijitkhan99git <arijitkhan52@gmail.com>
2025-10-28 13:20:03 +0530

    updated import files

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,10 @@
-import jordan from '@/assets/header/jordan.png'
-import nikeLogo from '@/assets/header/nikeLogo.png'
-import banner1 from '@/assets/banner/banner1.png'
-import airmax1 from '@/assets/air/airmax1.png'
-import airmax2 from '@/assets/air/airmax2.png'
-import airmax3 from '@/assets/air/airmax3.png'
-import airforce1 from '@/assets/air/airforce1.png'
-import airforce2 from '@/assets/air/airforce2.png'
+import jordan from './header/jordan.png'
+import nikeLogo from './header/nikeLogo.png'
+import banner1 from './banner/banner1.png'
+import airmax1 from './air/airmax1.png'
+import airmax2 from './air/airmax2.png'
+import airmax3 from './air/airmax3.png'
+import airforce1 from './air/airforce1.png'
+import airforce2 from './air/airforce2.png'
 
 export {jordan, nikeLogo, banner1, airmax1, airmax2, airmax3, airforce1, airforce2}
