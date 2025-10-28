773d500858f994d0746a3c5c12e944b1126667f2
Arijitkhan99git <arijitkhan52@gmail.com>
2025-10-28 12:18:06 +0530

    updated image file name

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,9 @@
 import jordan from '@/assets/header/jordan.png'
 import nikeLogo from '@/assets/header/nikeLogo.png'
 import banner1 from '@/assets/banner/banner1.png'
-import airMax1 from '@/assets/air/airmax1.png'
-import airMax2 from '@/assets/air/airmax2.png'
-import airMax3 from '@/assets/air/airmax3.png'
+import airMax1 from '@/assets/air/airMax1.png'
+import airMax2 from '@/assets/air/airMax2.png'
+import airMax3 from '@/assets/air/airMax3.png'
 import aiForce1 from '@/assets/air/airForce1.png'
 import airForce2 from '@/assets/air/airForce2.png'
 
